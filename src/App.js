fbe62355a50ddd36397ada312b83740bad0f6ffa
nvashish123 <nitin.vashi@gmail.com>
2019-04-27 17:52:50 -0400

    changed the greeting message

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import React from "react";
 function App() {
   return (
     <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
-      <h1 className="code f2-l">Notes App</h1>
-      Hello Atlanta!
+      <h1 className="code f2-l">Hello Atlanta!</h1>
+      Notes App!
     </div>
   );
 }
