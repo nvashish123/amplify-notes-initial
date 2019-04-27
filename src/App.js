80685b3eb609237d744ee14b5f0175b1836f20d1
nvashish123 <nitin.vashi@gmail.com>
2019-04-27 17:42:01 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
+      <h1 className="code f2-l">Notes App</h1>
+      Hello Atlanta!
     </div>
   );
 }
