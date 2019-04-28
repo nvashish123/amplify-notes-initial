c4ed2678938ad5f2aaee409ba1ad44986bb04f88
nvashish123 <nitin.vashi@gmail.com>
2019-04-28 09:27:51 -0400

    added UI controls

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import React, { Component } from "react";
+import { withAuthenticator } from "aws-amplify-react";
+
 class App extends Component {
   state = {
-    notes: [{}]
+    Note: "",
+    notes: []
   };
+
+  handleChangeNote = event => this.setState({ Note: event.target.value });
+  handleAddNote = async event => {
+    event.preventDefault();
+    const { Note, notes } = this.state;
+    const input = { Note };
+  };
+
   render() {
-    const { notes } = this.state;
+    const { notes, Note } = this.state;
     return (
       <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
         <h1 className="code f2-l">AWS Amplify App</h1>
 
         {/* notes form */}
-        <form className="mb3">
-          <input type="text" className="pa2 f4" placeholder="enter note" />
+        <form onSubmit={this.handleAddNote} className="mb3">
+          <input
+            type="text"
+            className="pa2 f4"
+            placeholder="enter note"
+            onChange={this.handleChangeNote}
+            value={Note}
+          />
           <button type="submit" className="pa2 f4">
             Add Note
           </button>
@@ -20,7 +37,7 @@ class App extends Component {
         <div>
           {notes.map(item => (
             <div key={item.id} className="flex items-center">
-              <li className="list pa1 f3">{item.note}</li>
+              <li className="list pa1 f3">{item.Note}</li>
               {/* <button className="bg-tranparent bn f4">
                  <span>&times;</span> 
               </button>*/}
@@ -32,4 +49,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withAuthenticator(App, { includeGreetings: true });
