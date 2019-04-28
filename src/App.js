a388898bfe765250ccb0c22232e01ce418d9af9f
nvashish123 <nitin.vashi@gmail.com>
2019-04-28 08:00:01 -0400

    modified uI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
+//import { withAuthenticator } from "aws-amplify-react";
+class App extends Component {
+  state = {
+    notes: [{}]
+  };
+  render() {
+    const { notes } = this.state;
+    return (
+      <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
+        <h1 className="code f2-l">AWS Amplify App</h1>
 
-function App() {
-  return (
-    <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
-      <h1 className="code f2-l">Hello Atlanta!</h1>
-      Notes App!
-    </div>
-  );
+        {/* notes form */}
+        <form className="mb3">
+          <input type="text" className="pa2 f4" placeholder="enter note" />
+          <button type="submit" className="pa2 f4">
+            Add Note
+          </button>
+        </form>
+        {/* Notes List */}
+        <div>
+          {notes.map(item => (
+            <div key={item.id} className="flex items-center">
+              <li className="list pa1 f3">{item.note}</li>
+              {/* <button className="bg-tranparent bn f4">
+                 <span>&times;</span> 
+              </button>*/}
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
+//export default withAuthenticator(App, { includeGreetings: true });
 export default App;
